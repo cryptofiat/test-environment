98932501980d0609f96c9f77f7abde2ece15c8bc
Maido Käära <maido@transferwise.com>
2017-03-19 14:31:18 -0400

    Run config server with contract data

--- build.js
+++ build.js
@@ -1,3 +1,5 @@
+const execSync = require('child_process').execSync;
+const yaml = require('js-yaml');
 const fs = require('fs');
 const solc = require('solc');
 const Web3 = require('web3');
@@ -20,22 +22,25 @@ const contracts = {};
 
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
-const contractSource = fs.readFileSync('../contract/CryptoFiat.sol', 'utf8');
+var compileContracts = function() {
+    const contractSource = fs.readFileSync('../contract/CryptoFiat.sol', 'utf8');
 
-const output = solc.compile(contractSource);
+    const output = solc.compile(contractSource);
 
-if (output.errors) {
-    output.errors.forEach(console.error);
-    process.exit(1);
-}
+    if (output.errors) {
+        output.errors.forEach(console.error);
+        process.exit(1);
+    }
+    console.log('Contracts compiled successfully!');
+    return output;
+};
 
-console.log('Contracts compiled successfully!');
+const output = compileContracts();
 
 var unlockAccount = function(account) {
     try {
         web3.personal.unlockAccount(account, 'Parool123');
     } catch (e) {
-        console.error(e);
     }
 };
 
@@ -79,7 +84,7 @@ var installContract = function(name, params) {
 var approveAccount = function(approver, account) {
     return function() {
         console.log('Approving account ' + account);
-        return new Promise(function(resolve, reject){
+        return new Promise(function(resolve, reject) {
             unlockAccount(approver);
             contracts['Approving'].approveAccount(account, {from: approver, gas: 50000000}, function(err, result) {
                 if(err) {
@@ -108,26 +113,93 @@ var upgrade = function(CryptoFiat, accounts, id) {
     };
 };
 
-installContract('CryptoFiat')().then(function(c) {
-    web3.eth.getAccounts(function (e, accounts) {
-            installContract('Data', [c.address])()
-            .then(upgrade(c, accounts, 1))
-            .then(installContract('Accounts', [c.address]))
-            .then(upgrade(c, accounts, 2))
-            .then(installContract('Approving', [c.address, accounts[roles.AccountApprover]]))
-            .then(upgrade(c, accounts, 3))
-            .then(installContract('Reserve', [c.address, accounts[roles.Reserve]]))
-            .then(upgrade(c, accounts, 4))
-            .then(installContract('Enforcement', [c.address, accounts[roles.LawEnforcer], accounts[roles.Designator], accounts[roles.Account]])).then(upgrade(c, accounts, 5))
-            .then(installContract('AccountRecovery', [c.address]))
-            .then(upgrade(c, accounts, 6))
-            .then(installContract('Delegation', [c.address]))
-            .then(upgrade(c, accounts, 7))
-            .then(function () {
-                console.log("All contracts mined and registered! Approving accounts");
-                approveAccount(accounts[roles.AccountApprover], accounts[roles.Alice])()
-                .then(approveAccount(accounts[roles.AccountApprover], accounts[roles.Bob]))
-                .then(approveAccount(accounts[roles.AccountApprover], accounts[roles.Carol]));
+var increaseSupply = function(accounts, amount) {
+    return function() {
+        console.log('Increasing CryptoFiat reserve supply by ' + amount);
+        return new Promise(function(resolve, reject) {
+            unlockAccount(accounts[roles.Reserve]);
+            contracts['Reserve'].increaseSupply(amount, {from: accounts[roles.Reserve], gas: 50000000}, function(err, result) {
+                if(err) {
+                    reject(err);
+                } else {
+                    console.log('Reserve supply increased by ' + amount);
+                    resolve(result);
+                }
             });
+        });
+    };
+};
+
+var transfer = function(from, to, amount) {
+    return function() {
+        console.log('Transferring ' + amount + ' of CryptoFiat from ' + from + ' to ' + to);
+        return new Promise(function(resolve, reject) {
+            unlockAccount(from);
+            contracts['Accounts'].transfer(to, amount, {from: from, gas: 50000000}, function(err, result) {
+                if(err) {
+                    reject(err);
+                } else {
+                    console.log(amount + ' of CryptoFiat from ' + from + ' to ' + to + ' transferred!');
+                    resolve(result);
+                }
+            });
+        });
+    };
+};
+
+var generateConfigFile = function(contracts, accounts) {
+    console.log('Generating config file');
+    const CryptoFiat = contracts['CryptoFiat'];
+    const config = {
+        "sponsor": {
+            "ethereum": {
+                "address": accounts[roles.Reserve]
+            }
+        },
+        "contract": {
+            "ethereum": {
+                "address": CryptoFiat.address,
+                "block": web3.eth.getTransaction(CryptoFiat.transactionHash).blockNumber
+            }
+        },
+        "ethereum": {
+            "node": {
+                "url": 'http://localhost:8545'
+            }
+        }
+    };
+    fs.writeFileSync('application.yml', yaml.safeDump(config));
+};
+
+var installContracts = function() {
+    installContract('CryptoFiat')().then(function(c) {
+        web3.eth.getAccounts(function (e, accounts) {
+                installContract('Data', [c.address])()
+                .then(upgrade(c, accounts, 1))
+                .then(installContract('Accounts', [c.address]))
+                .then(upgrade(c, accounts, 2))
+                .then(installContract('Approving', [c.address, accounts[roles.AccountApprover]]))
+                .then(upgrade(c, accounts, 3))
+                .then(installContract('Reserve', [c.address, accounts[roles.Reserve]]))
+                .then(upgrade(c, accounts, 4))
+                .then(installContract('Enforcement', [c.address, accounts[roles.LawEnforcer], accounts[roles.Designator], accounts[roles.Account]])).then(upgrade(c, accounts, 5))
+                .then(installContract('AccountRecovery', [c.address]))
+                .then(upgrade(c, accounts, 6))
+                .then(installContract('Delegation', [c.address]))
+                .then(upgrade(c, accounts, 7))
+                .then(function () {
+                    console.log("All contracts mined and registered! Approving accounts");
+                    approveAccount(accounts[roles.AccountApprover], accounts[roles.Alice])()
+                    .then(approveAccount(accounts[roles.AccountApprover], accounts[roles.Bob]))
+                    .then(approveAccount(accounts[roles.AccountApprover], accounts[roles.Carol]))
+                    .then(increaseSupply(accounts, 10000))
+                    .then(transfer(accounts[roles.Reserve], accounts[roles.Alice], 1000))
+                    .then(transfer(accounts[roles.Reserve], accounts[roles.Bob], 1000))
+                    .then(transfer(accounts[roles.Reserve], accounts[roles.Carol], 1000)).then(function() {
+                        console.log('Setup finished successfully! Generating config file');
+                        generateConfigFile(contracts, accounts);
+                    });
+                });
+        });
     });
-});
+};
